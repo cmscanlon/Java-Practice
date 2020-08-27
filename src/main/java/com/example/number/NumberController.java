41141e8b9c6e0fabe1e0fdc6227ce724d96e03dc
Carolyn <cmscanlon@yahoo.com>
2020-08-27 10:13:54 -0500

    Adding more projects and tests -CMS

--- src/main/java/com/example/number/NumberController.java
+++ src/main/java/com/example/number/NumberController.java
@@ -20,14 +20,49 @@ public class NumberController {
         }
     }
 
-    @RequestMapping("/random")
-    public int getRandomNumber() {
-        int number = (int) (Math.random() * 10);
+    @GetMapping("/odd")
+    public int getIsOdd(@RequestParam(value = "first-number") Integer firstNumber, @RequestParam(value = "second-number") Integer secondNumber) {
 
-        if (number != 7) {
-            return 102;
+        int answer = (firstNumber + secondNumber) % 2;
+
+        if (answer == 1) {
+            return 200;
         } else {
-            return 107;
+            return 207;
         }
     }
+
+    @GetMapping("/palindrome")
+    public String getIsPalindrome(@RequestParam(value = "word") String word) {
+
+        int number = word.length();
+        for (int i = 0; i < (number/2); i++) {
+            if (word.charAt(i) != word.charAt(number - i - 1)) {
+                return "Not a palindrome";
+            }
+        }
+        return "It is a palindrome!";
+    }
+
+    @GetMapping("/leap")
+    public int getIsLeapYear(@RequestParam(value = "year") Integer year) {
+        boolean isLeap = false;
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0)
+                    isLeap = true;
+                else
+                    isLeap = false;
+            } else
+                isLeap = true;
+        } else {
+            isLeap = false;
+        }
+        if(isLeap == true)
+            return 200;
+        else
+            return 207;
+    }
+
 }
--- src/test/java/com/example/number/NumberControllerTest.java
+++ src/test/java/com/example/number/NumberControllerTest.java
@@ -10,13 +10,28 @@ public class NumberControllerTest {
     @Test
     public void isEvenTest() {
         NumberController number = new NumberController();
-        assertEquals(200, number.getIsEven(1,5));
-        assertEquals(207, number.getIsEven(4,3));
+        assertEquals(200, number.getIsEven(1, 5));
+        assertEquals(207, number.getIsEven(4, 3));
     }
 
     @Test
-    public void RandomNumberTest() {
-        NumberController random = new NumberController();
-        assertNotEquals(10, random.getRandomNumber());
+    public void isOddTest() {
+        NumberController number = new NumberController();
+        assertEquals(200, number.getIsOdd(1,4));
+        assertEquals(207, number.getIsOdd(1,5));
+    }
+
+    @Test
+    public void isPalindrome() {
+        NumberController palindrome = new NumberController();
+        assertEquals("It is a palindrome!", palindrome.getIsPalindrome("racecar"));
+        assertEquals("Not a palindrome", palindrome.getIsPalindrome("racecars"));
+    }
+
+    @Test
+    public void isLeapYear() {
+        NumberController leapYear = new NumberController();
+        assertEquals(200, leapYear.getIsLeapYear(2020));
+        assertEquals(207, leapYear.getIsLeapYear(1999));
     }
 }
